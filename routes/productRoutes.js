f2f572a1ee955b1fb1377e1e069e954d6289fd3d
Gasorekibo <m.gasore@alustudent.com>
2024-09-20 16:03:25 +0200

    feat: add product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProducts } from "../controllers/productController.js";
+import { addProduct, getProducts } from "../controllers/productController.js";
 const productRoute = express.Router();
 
 
 productRoute.get("/", getProducts);
+productRoute.post("/add", addProduct);
 
 
 export default productRoute;
