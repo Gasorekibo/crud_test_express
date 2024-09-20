5f369b7adf6e62ea48ca63fea92a10dce1f50c5e
Gasorekibo <m.gasore@alustudent.com>
2024-09-20 16:01:11 +0200

    feat:delete a product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,4 +55,13 @@ export const updateProduct = async (req, res) => {
   }
 };
 
-
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) return res.status(404).json('Product not found');
+    return res.status(200).json('Product Deleted');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
