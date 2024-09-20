4e1a3df7cb7a7aab0604bf021c12e34c771cc8a3
Gasorekibo <m.gasore@alustudent.com>
2024-09-20 16:04:53 +0200

    feat: update product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addProduct, getOneProduct, getProducts } from "../controllers/productController.js";
+import { addProduct, getOneProduct, getProducts, updateProduct } from "../controllers/productController.js";
 const productRoute = express.Router();
 
 
 productRoute.get("/", getProducts);
 productRoute.post("/add", addProduct);
 productRoute.get("/:id",getOneProduct);
+productRoute.put("/:id",updateProduct);
 
 
 export default productRoute;
