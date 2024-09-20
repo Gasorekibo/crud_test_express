8725f816510da6ef0407e89f63ac7e17b34872a8
Gasorekibo <m.gasore@alustudent.com>
2024-09-20 16:03:57 +0200

    feat: get single product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addProduct, getProducts } from "../controllers/productController.js";
+import { addProduct, getOneProduct, getProducts } from "../controllers/productController.js";
 const productRoute = express.Router();
 
 
 productRoute.get("/", getProducts);
 productRoute.post("/add", addProduct);
+productRoute.get("/:id",getOneProduct);
 
 
 export default productRoute;
