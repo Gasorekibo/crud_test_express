4f9ca1a921f1c21bcbd792c4e759d8007d6d626b
Gasorekibo <m.gasore@alustudent.com>
2024-09-20 15:59:35 +0200

    feat:add new product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 import Product from '../model/product.js';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getProducts = async (req, res) => {
   try {
@@ -6,6 +7,23 @@ export const getProducts = async (req, res) => {
     if (!product || product.length < 1)
       return res.status(404).json('No Products');
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const addProduct = async (req, res) => {
+  const { name, price, description } = req.body;
+  try {
+    const isProductExists = await Product.findOne({ name });
+    if (isProductExists) return res.status(301).json('Product already Exists');
+    const newProduct = await Product.create({
+      id: uuidv4(),
+      name,
+      price,
+      description,
+    });
+    if (newProduct) return res.status(201).json(newProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
